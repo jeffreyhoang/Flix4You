1005d2a3d77908b01e4ea7bcd9498fa9890f2dea
Alexander <alexanderlee389@gmail.com>
2025-02-19 00:45:19 -0800

    Updating backend stuff
    Updating backend stuff

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import list_users
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),  # Includes all API routes from `api/urls.py`
-    path('api/users/', list_users, name='list_users'),
 ]
