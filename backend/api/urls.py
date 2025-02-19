175ed545060bbb416a45d1264648f35c0228db7e
Alexander <alexanderlee389@gmail.com>
2025-02-19 01:04:57 -0800

    updated urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import SignupView
+from .views import SignupView, list_users  
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("signup/", SignupView.as_view(), name="signup"),  # User Signup
     path("token/", TokenObtainPairView.as_view(), name="get_token"),  # Login (JWT Auth)
     path("token/refresh/", TokenRefreshView.as_view(), name="refresh"),  # Refresh Token
+    path("users/", list_users, name="list_users"),  
 ]
