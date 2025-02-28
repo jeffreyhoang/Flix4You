0f369f80a1387e1bc0a36c21281aa3afa3a09739
Alexander <alexanderlee389@gmail.com>
2025-02-28 11:07:14 -0800

    updating models

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Director, Actor, Movie, UserProfile, Watchlist, History, Ratings
 
-# Register your models here.
+# Register Models
+admin.site.register(Director)
+admin.site.register(Actor)
+admin.site.register(Movie)
+admin.site.register(UserProfile)
+admin.site.register(Watchlist)
+admin.site.register(History)
+admin.site.register(Ratings)
