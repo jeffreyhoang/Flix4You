3269cd194a4a8267cbc808127fa192c36b66acfd
jeffreyhoang <148601460+jeffreyhoang@users.noreply.github.com>
2025-03-03 17:59:02 -0800

    Update api.jsx

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Change this URL if needed (use localhost for local testing)
-// const API_BASE_URL = "http://127.0.0.1:8000/api";
+// const API_BASE_URL = "http://127.0.0.1:8000/api";   
 const API_BASE_URL = "https://flix4u-production.up.railway.app/";
 
 // Fetch all users
