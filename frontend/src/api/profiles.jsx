07f616df9ce8844505ad190dddd73aba1086c66b
jeffreyhoang <148601460+jeffreyhoang@users.noreply.github.com>
2025-03-11 20:02:06 -0700

    fixed frontend api connection

--- frontend/src/api/profiles.jsx
+++ frontend/src/api/profiles.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 // Change this URL if needed (use localhost for local testing)
-const API_BASE_URL = "http://127.0.0.1:8000/api";   
-//const API_BASE_URL = "https://flix4u-production.up.railway.app/api";
+const API_BASE_URL = "http://127.0.0.1:8000/api/";   
+//const API_BASE_URL = "https://flix4u-production.up.railway.app/api/";
 
 // Get Authenticated User Details
 export const getUser = async (token) => {
@@ -11,13 +11,38 @@ export const getUser = async (token) => {
     });
 };
 
-// Create a New Profile
-
 // List User Profiles
+export const getProfiles = async (token) => {
+    return axios.get(`${API_BASE_URL}profile/`, {
+        headers: { Authorization: `Bearer ${token}`}
+    });
+};
 
-// Update a Profile
+// Create a New Profile
+ export const createProfile = async (token, profileData) => {
+    return axios.get(`${API_BASE_URL}profile/`, profileData, {
+        headers: { Authorization: `Bearer ${token}` }  
+    });
+ };
 
-// Retrive a Profile
+// Update a User Profile
+export const updateProfile = async (token, profileId, updatedData) => {
+    return axios.put(`${API_BASE_URL}profile/${profileId}/`, updatedData, {
+        headers: { Authorization: `Bearer ${token}` }
+    });
+};
 
-// Delete a Profile
+// Retrieve a User Profile
+export const getProfile = async (token, profileId) => {
+    return axios.get(`${API_BASE_URL}profile/${profileId}/`, {
+        headers: { Authorization: `Bearer ${token}`}
+    });
+};
+
+// Delete a User Profile
+export const deleteProfile = async (token, profileId) => {
+    return axios.delete(`${API_BASE_URL}profile/${profileId}/`, {
+        headers: { Authorization: `Bearer ${token}`}
+    });
+};
 
