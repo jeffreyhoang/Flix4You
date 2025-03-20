d3f8fd3f20f09ae8cc38326934042f22e60d5e74
jeffreyhoang <148601460+jeffreyhoang@users.noreply.github.com>
2025-03-20 12:40:50 -0700

    u

--- backend/movies/models.py
+++ backend/movies/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+class Genre(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+    
 class Actor(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
@@ -7,12 +13,6 @@ class Actor(models.Model):
         return self.name
 
 class Director(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-
-    def __str__(self):
-        return self.name
-    
-class Genre(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
